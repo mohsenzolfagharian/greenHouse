b788313c8134fa89168fe2ca92d4b0c82f352a34
mohsen Zolfagharian <mohsen.zolfagharian.98@gmail.com>
2022-04-04 12:58:01 +0430

    Update Post_data.py

--- Post_data.py
+++ Post_data.py
@@ -1,16 +1,12 @@
 import requests
 import FinallValues
-#import Get
 from time import sleep
 
 
 postURL = "http://0ebfd90d5509.ngrok.io/api/v1/sensorlog"
 
 while True:
-#    y = Get.get_Status()
     x = FinallValues.SensorsData()
-#    x["lamp_status"] = y["lamp_status"]
-#    x["pomp_status"] = y["pomp_status"]
     r = requests.post(postURL,x)
     print(r.status_code, x)
     sleep(3)
